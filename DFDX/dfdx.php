f76da468652cd11fabebff972c39881ca91f4610
dfdxAlex <alexmway@mail.ru>
2022-01-09 22:31:17 +0100

    testFoVideo

--- DFDX/dfdx.php
+++ DFDX/dfdx.php
@@ -13,7 +13,6 @@ include "image/swapImages.php";
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-MF3F7YTKCQ');
 </script>
 <meta charset="UTF-8">
@@ -28,7 +27,6 @@ echo '<link rel="stylesheet" href="'.$b->searcNamePath('dfdx.css').'"> ';
 ?>
 <meta name="Cache-Control" content="no-store">
 </head>
- 
 <body>
 <?php
 if (!isset($_SESSION["resetNameTable"])) $_SESSION["resetNameTable"]=false;
@@ -40,7 +38,6 @@ if (!isset($_SESSION['redaktiruem'])) $_SESSION['redaktiruem']='';
 $redaktor=new redaktor\modul();
 $status = new redaktor\login();
 $maty = new redaktor\maty();
-
 if ($_SESSION["status"]>99) $_SESSION["status"]=9;
 if (isset($_POST['redaktor_up'])) $_SESSION["regimRaboty"]=0; // Если пришли из редактора движка, то абнулить режим работы
 ////////////////////////////////////////////Верхнее меню///////////////////////////////////////////////////////   
@@ -48,34 +45,33 @@ if (isset($_POST['redaktor_up'])) $_SESSION["regimRaboty"]=0; // Если при
 ///////////////////////////////////////////Обработка верхнего меню
 if ($_SESSION["status"]>0)             // если есть какой-то статух входа на сайт
  if (isset($_POST['menu_up_dfdx']))    // если было нажатие любой кнопки верхнего меню
-  if ($_POST['menu_up_dfdx']=='Выход') // Если была нажата кнопка Выход верхнего меню
-   {
+  if ($_POST['menu_up_dfdx']=='Выход') {// Если была нажата кнопка Выход верхнего меню
     $_SESSION["status"]=0;              // Обнуляем статус пользователя (выходим)
     $_SESSION["login"]='';
-   }
+ }
 
 if ($_SESSION["status"]==0)             // если пользователь не вошел
   if (isset($_POST['menu_up_dfdx']))    // если было нажатие любой кнопки верхнего меню
-    if ($_POST['menu_up_dfdx']=='Вход') // Если была нажата кнопка Вход верхнего меню
-      {
+    if ($_POST['menu_up_dfdx']=='Вход') {// Если была нажата кнопка Вход верхнего меню
         $_SESSION["login"]=$_POST['login'];
         $_SESSION["parol"]=$_POST['parol'];
-      }
-if (isset($_SESSION["login"]) && isset($_SESSION["parol"])) $_SESSION["status"]=$status->statusRegi($_SESSION["login"],$_SESSION["parol"]);
+  }
+if (isset($_SESSION["login"]) && isset($_SESSION["parol"])) 
+    $_SESSION["status"]=$status->statusRegi($_SESSION["login"],$_SESSION["parol"]);
 echo '<section class="container-fluid">';
-echo '<div class="row">';
-echo '<div class="col-xl-4 col-lg-4 col-md-3 col-sm-12 col-12">';
-if (isset($_SESSION["login"]))
- {
-  echo '<div class="monetki"><img src="'.$b->searcNamePath('image/pngwingmal.png').'" class="img-fluid" alt="монет"></div>';
-  echo $redaktor->money('login='.$_SESSION["login"]);
- }
-echo '</div>';
-echo '<div class="col-xl-8 col-lg-8 col-md-9 col-sm-12 col-12">';
-if ($_SESSION["status"]>99) $_SESSION["status"]=9;
-$maty->__unserialize(array('menu9','menu_up_dfdx','dfdx.php','Логин','Пароль'));
-echo '</div>';
-echo '</div>';
+    echo '<div class="row">';
+        echo '<div class="col-xl-4 col-lg-4 col-md-3 col-sm-12 col-12">';
+            if (isset($_SESSION["login"])) {
+                echo '<div class="monetki"><img src="'.$b->searcNamePath('image/pngwingmal.png').'" class="img-fluid" alt="монет"></div>';
+                echo $redaktor->money('login='.$_SESSION["login"]);
+            }
+        echo '</div>';
+        echo '<div class="col-xl-8 col-lg-8 col-md-9 col-sm-12 col-12">';
+        if ($_SESSION["status"]>99) 
+            $_SESSION["status"]=9;
+        $maty->__unserialize(array('menu9','menu_up_dfdx','dfdx.php','Логин','Пароль'));
+        echo '</div>';
+    echo '</div>';
 echo '</section>';
 //$_SESSION['redaktiruem']="dfdx.php";
 ////////////////////////////Начало основного кода страницы//////////////////////////  
@@ -84,100 +80,91 @@ echo '  <img src="'.$b->searcNamePath('image/logo.png').'" alt="Картинка
  //////////////////////////////////////////////////////////////////////////////////////////////////
  // Раздел сайта показать
 echo '<section class="container-fluid">';
-echo '<div class="row">';
-echo '<div class="col-12">';
-echo '<div class="logoHtml">';
-if (!stripos('sss'.$_SERVER['REQUEST_URI'],'news')>0)
-{ 
-if (file_exists('image/home.png'))
- echo '<img src="image/home.png" alt="home">';
- else home();
-}
-// Блок работает тогда, когда данный файл вызывается из персональных ссылок для статей
-if (stripos('sss'.$_SERVER['REQUEST_URI'],'news')>0)
-{
-  $pathMas=preg_split('/news/',$_SERVER['REQUEST_URI']);
-  $pathFile='news'.$pathMas[1];
-  $zapros="SELECT bd2.name FROM bd2, url_po_id_bd2 WHERE bd2.id=url_po_id_bd2.id AND url_po_id_bd2.url='".$pathFile."'";
-  $rez=$maty->zaprosSQL($zapros);
-  if ($b->notFalseAndNULL($rez))
-    {
-        $stroka=mysqli_fetch_array($rez);
-        zagolowkaBeg($stroka[0]);
-    }
-}
+    echo '<div class="row">';
+        echo '<div class="col-12">';
+            echo '<div class="logoHtml">';
+            if (!stripos('sss'.$_SERVER['REQUEST_URI'],'news')>0) { 
+                if (file_exists('image/home.png'))
+                    echo '<img src="image/home.png" alt="home">';
+                else home();
+            }
+            // Блок работает тогда, когда данный файл вызывается из персональных ссылок для статей
+            if (stripos('sss'.$_SERVER['REQUEST_URI'],'news')>0) {
+                $pathMas=preg_split('/news/',$_SERVER['REQUEST_URI']);
+                $pathFile='news'.$pathMas[1];
+                $zapros="SELECT bd2.name FROM bd2, url_po_id_bd2 WHERE bd2.id=url_po_id_bd2.id AND url_po_id_bd2.url='".$pathFile."'";
+                $rez=$maty->zaprosSQL($zapros);
+                if ($b->notFalseAndNULL($rez)) {
+                    $stroka=mysqli_fetch_array($rez);
+                    zagolowkaBeg($stroka[0]);
+                }
+            }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-echo '<hr>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
+            echo '<hr>';
+            echo '</div>';
+        echo '</div>';
+    echo '</div>';
 echo '</section>';
 //////////////////////////////////////////////////////////////////////////////
 echo '<section class="container-fluid pole">';
-echo '<div class="row">';
-echo '<div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-12">';  // Левое меню
-levoeMenu();
-echo '</div>';
+    echo '<div class="row">';
+        echo '<div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-12">';  // Левое меню
+        levoeMenu();
+        echo '</div>';
 
-echo '<div class="col-xl-8 col-lg-8 col-md-9 col-sm-8 col-12">';  // Центр
-$bylPoisk=false;
-$poisk = new \redaktor\poisk();
-$redaktor=new redaktor\modul();
-$action='action=dfdx.php';  //страница обработки кнопок в модуле news()
-$runNewsIsNews1=-1;
-$metka='dfdx'; //метка для счётчика статистики посещения конкретной страницы
-$nomerNewsPoisk='Число_статей=5';
-$nomerNewsGlawn='Число_статей=5';
-//if (!file_exists($action)) $action=basename(__FILE__);
+        echo '<div class="col-xl-8 col-lg-8 col-md-9 col-sm-8 col-12">';  // Центр
+        $bylPoisk=false;
+        $poisk = new \redaktor\poisk();
+        $redaktor=new redaktor\modul();
+        $action='action=dfdx.php';  //страница обработки кнопок в модуле news()
+        $runNewsIsNews1=-1;
+        $metka='dfdx'; //метка для счётчика статистики посещения конкретной страницы
+        $nomerNewsPoisk='Число_статей=5';
+        $nomerNewsGlawn='Число_статей=5';
+        //if (!file_exists($action)) $action=basename(__FILE__);
 
-////////////////////////////////////////////////////поиск
-if (isset($_POST['poisk']))
- {
-  $poisk->poiskStati('bd2',$_POST['strPoisk'],$idStati) ;
-  if ($idStati[0]>-1)
-    foreach($idStati as $value) 
-     $redaktor->news1("nameTD=bd2","Заголовок=h3","Статус редактора=-s12345","Шаблон=2","Отступ=1",$action,'id='.$value,$nomerNewsPoisk);
-    $bylPoisk=true;
- }
-///////////////////////////////////////////////////
-//маркер
- if (!$bylPoisk)
- {
-              // echo 'ловим кнопку';
-               $statiaPoId=$maty->hanterButton("false=netKnopki","rez=hant","nameStatic=panelPrawa","returnNameDynamic");
-               //echo 'стр.148-'.$statiaPoId.'<br>';
-               if ($statiaPoId=='netKnopki' )  // Если не была нажата кнопка правой панели проверяем нажатие заголовков статей
-                  $statiaPoId=$maty->hanterButton("false=netKnopki","rez=hant","nameStatic=statiaKorotka","returnNameDynamic");
-               //echo 'стр.151-'.$statiaPoId.'<br>';
+        ////////////////////////////////////////////////////поиск
+        if (isset($_POST['poisk'])) {
+            $poisk->poiskStati('bd2',$_POST['strPoisk'],$idStati) ;
+            if ($idStati[0]>-1)
+                foreach($idStati as $value) 
+                    $redaktor->news1("nameTD=bd2","Заголовок=h3","Статус редактора=-s12345","Шаблон=2","Отступ=1",$action,'id='.$value,$nomerNewsPoisk);
+                $bylPoisk=true;
+         }
+        ///////////////////////////////////////////////////
+        //маркер
+        if (!$bylPoisk) {
+            // echo 'ловим кнопку';
+            $statiaPoId=$maty->hanterButton("false=netKnopki","rez=hant","nameStatic=panelPrawa","returnNameDynamic");
 
-              if (isset($_SESSION['statiaPoId']))
+            if ($statiaPoId=='netKnopki' )  // Если не была нажата кнопка правой панели проверяем нажатие заголовков статей
+                $statiaPoId=$maty->hanterButton("false=netKnopki","rez=hant","nameStatic=statiaKorotka","returnNameDynamic");
+
+            if (isset($_SESSION['statiaPoId']))
                if ($statiaPoId=='netKnopki') 
-                $statiaPoId=$_SESSION['statiaPoId'];
+                  $statiaPoId=$_SESSION['statiaPoId'];
 
-              if ($statiaPoId=='netKnopki' || isset($_POST['menu_up_dfdx']))  // Если не была нажата кнопка правой панели
-                $redaktor->news1("nameTD=bd2","Заголовок=h3","Статус редактора=-s12345","Шаблон=2","Отступ=1",$action,$nomerNewsGlawn);
+            if ($statiaPoId=='netKnopki' || isset($_POST['menu_up_dfdx']))  // Если не была нажата кнопка правой панели
+               $redaktor->news1("nameTD=bd2","Заголовок=h3","Статус редактора=-s12345","Шаблон=2","Отступ=1",$action,$nomerNewsGlawn);
 
             //НЕ УДАЛЯТЬ, ИСПОЛЬЗУЕТСЯ в персональных ссылках
             //if (!$_SESSION["runStrNews"]) $statiaPoId=$runNewsIsNews1; 
 
-              if ($statiaPoId>-1 && !isset($_POST['menu_up_dfdx']) && $statiaPoId!='netKnopki')
-                 $redaktor->news1("id=".$statiaPoId,"nameTD=bd2","Заголовок=h3","Статус редактора=-s12345","Шаблон=2","Отступ=1",$action,$nomerNewsGlawn);
- }
-  //buttonTwitter
-  $_SESSION["runStrNews"]=false; // обнуление переменной
-echo '</div>';
-//////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
-echo '<div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-12 prawy">';  // правое меню
-
-echo '<div class="poiskDiv">';
-poiskDfdx('dfdx.php');
-echo '</div>';
-
-pravoePole("home");   // категория статей, которые должны быть показаны в правом меню 
-echo '</div>';
-
-echo '</div>';
+            if ($statiaPoId>-1 && !isset($_POST['menu_up_dfdx']) && $statiaPoId!='netKnopki')
+               $redaktor->news1("id=".$statiaPoId,"nameTD=bd2","Заголовок=h3","Статус редактора=-s12345","Шаблон=2","Отступ=1",$action,$nomerNewsGlawn);
+          }
+          //buttonTwitter
+          $_SESSION["runStrNews"]=false; // обнуление переменной
+          echo '</div>';
+          /////////////////////////////////////////////////////////////////////////////////////
+          //////////////////////////////////////////////////////////////////////////////////////
+          echo '<div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-12 prawy">';  // правое меню
+              echo '<div class="poiskDiv">';
+              poiskDfdx('dfdx.php');
+          echo '</div>';
+          pravoePole("home");   // категория статей, которые должны быть показаны в правом меню 
+        echo '</div>';
+    echo '</div>';
 echo '</section>';
 ////////////////////////////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////Конец основного кода страницы////////////////////////// 
@@ -185,11 +172,10 @@ echo '</section>';
 echo '<footer class="container-fluid futter">';
 $statistik = new redaktor\statistic();
 if ($_SESSION['regimRaboty']==22) // исполнение нажатия кнопки Статистика
-$statistik->statistikOnOff();
+    $statistik->statistikOnOff();
 
 if ($_SESSION['regimRaboty']==21) //исполнение нажатия Маты
-$maty->redactMaty();
-
+    $maty->redactMaty();
 
 // Вывод статистики Футтер
 $statistik->metkaStatistika($metka);
