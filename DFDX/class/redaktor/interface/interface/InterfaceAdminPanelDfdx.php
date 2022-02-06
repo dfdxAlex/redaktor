b7a28569fe5b5d67c04a67c8530918420f2c0269
dfdxAlex <alexmway@mail.ru>
2022-02-06 20:22:37 +0100

    testFoVideo

---
+++ DFDX/class/redaktor/interface/interface/InterfaceAdminPanelDfdx.php
@@ -0,0 +1,11 @@
+<?php
+namespace class\redaktor\interface\interface;
+
+// интерфейс для функций, реализующих работу административной панели CMS-DFDX
+// interfejs dla funkcji realizujących pracę panelu administracyjnego CMS-DFDX
+// interface for functions that implement the work of the CMS-DFDX administrative panel
+
+
+interface InterfaceAdminPanelDfdx extends InterfaceWorkToMenu
+{
+}
