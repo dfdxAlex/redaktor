5ce536c94960e48d53297bb3c281639faabe775b
dfdxAlex <alexmway@mail.ru>
2022-02-07 20:11:43 +0100

    testFoVideo

--- DFDX/class/redaktor/interface/interface/InterfaceWorkToFutter.php
+++ DFDX/class/redaktor/interface/interface/InterfaceWorkToFutter.php
@@ -7,4 +7,13 @@ namespace class\redaktor\interface\interface;
 
 interface InterfaceWorkToFutter extends InterfaceWorkToMenu
 {
+    // функция выводит нижний блок сайта
+    // funkcja wyświetla dolny blok strony
+    // function displays the bottom block of the site
+    public function futterR(...$parametr);
+
+    // функция выводит дату редактирования сайта, Не Работает
+    // funkcja wyświetla datę edycji strony, Nie działa
+    // function displays the date the site was edited, Doesn't work
+    public function dataRedaktSite();
 }
