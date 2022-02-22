e92ad18759d789eb4c607d9a9fdb20fa7086da96
dfdxAlex <alexmway@mail.ru>
2022-02-23 00:12:28 +0100

    testFoVideo

---
+++ DFDX/class/redaktor/interface/interface/InterfaceWorkToMail.php
@@ -0,0 +1,10 @@
+<?php
+namespace class\redaktor\interface\interface;
+
+//интерфейс функций, работающих с почтой
+//interfejs funkcji obsługujących pocztę
+//interface of functions working with mail
+
+interface InterfaceWorkToMail
+{
+}
