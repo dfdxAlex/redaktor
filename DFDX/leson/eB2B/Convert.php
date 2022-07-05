79b5ed30913606b65f28c9930f9385bb68b0d4ac
dfdxAlex <alexmway@mail.ru>
2022-07-05 20:50:45 +0200

    b2b

--- DFDX/leson/eB2B/Convert.php
+++ DFDX/leson/eB2B/Convert.php
@@ -8,6 +8,23 @@ class Convert
 
     }
 
+    public function convertToString(string $str='')
+    {
+        if ($str=='') return false;
+
+        $strRezult='';
+        //przekonwertuj ciąg na tablicę
+        $masInt=explode(',', $str);
+
+        //iteruj po wszystkich elementach tablicy
+        foreach ($masInt as $value) 
+            //dodaj znak do łańcucha według jego numeru z tablicy
+            $strRezult.=$this->intToChar($value);
+
+        // zwróć wynikowy ciąg
+        return $strRezult;
+    }
+
     public function convertToNumeric(string $str='')
     {
         if ($str=='') return false;
@@ -32,9 +49,43 @@ class Convert
         return $strRezult;
     }
 
+    //zmień numer na symbol
+    function intToChar(int $v)
+    {
+        return match ($v) {
+            2   =>'a',
+            22  =>'b',
+            222 =>'c',
+            3   =>'d',
+            33  =>'e',
+            333 =>'f',
+            4   =>'g',
+            44  =>'h',
+            444 =>'i',
+            5   =>'j',
+            55  =>'k',
+            555 =>'l',
+            6   =>'m',
+            66  =>'n',
+            666 =>'o',
+            7   =>'p',
+            77  =>'q',
+            777 =>'r',
+            7777=>'s',
+            8   =>'t', 
+            88  =>'u',
+            888 =>'v',
+            9   =>'w',
+            99  =>'x',
+            999 =>'y',
+            9999=>'z',
+            0   =>' ',
+            default => ' ',
+        };  
+    }
 
     //zamiana jednego znaku na liczby
-    public function charToInt($v)
+    function charToInt(string $v)
     {
         return match (mb_strtolower($v)) {
             'a' => '2',
@@ -64,6 +115,7 @@ class Convert
             'y' => '999',
             'z' => '9999',
             ' ' => '0',
+            default => ' ',
         };  
     }
 }
--- DFDX/leson/eB2B/eB2B.php
+++ DFDX/leson/eB2B/eB2B.php
@@ -5,3 +5,5 @@ require "Convert.php";
 $a = new Convert();
 
 echo $a->convertToNumeric('Ela nie ma kota');
+
+echo $a->convertToString('5,2,22,555,33,222,9999,66,444,55');
