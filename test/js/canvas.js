6216d4995187da57f5717a6de7593c7a7dce15f2
dfdxAlex <alexmway@mail.ru>
2022-09-27 22:40:19 +0200

    TraitInterfaceButton

--- test/js/canvas.js
+++ test/js/canvas.js
@@ -5,32 +5,59 @@ function initiate()
 
     canvas5.beginPath();
 
-    //canvas5.moveTo(0,0);      // перенести перо в координаты
-    //canvas5.lineTo(50,50);    // рисовать от предыдущих координат к указанным
-    //canvas5.moveTo(70,70);
-    //canvas5.lineTo(100,100);
+    //canvas5.moveTo(x1,y1);      // перенести перо в координаты
 
-    //canvas5.moveTo(100,100);  // переносим перо на новое место
-    //canvas5.lineTo(220,220);  // рисуем одну линию
-    //canvas5.lineTo(280,120);  // рисуем вторую линию
-    //canvas5.lineTo(280,320);  // рисуем вторую линию
-    //canvas5.closePath();
-    //canvas5.fill();           // соединияе начало и конец и закрашиваем
+    // координаты начала
+    var x1=50;
+    var y1=50;
 
-    //canvas5.lineTo(0,0);
+    // Контрольная точка
+    var x3=450;
+    var y3=150;
+
+    // Контрольная точка
+    var x4=150;
+    var y4=350;
+
+    // координаты конца
+    var x2=450;
+    var y2=450;
+
+    // кубическая кривая Безье
+    canvas5.moveTo(x1,y1);
+
+    canvas5.lineWidth=15;       // Толщина линии
+    //canvas5.lineCap='butt';   // концы по умолчанию
+    //canvas5.lineCap='round';  // концы закруглены
+    //canvas5.lineCap='square'; // концы квадратами
+
+    //canvas5.lineJoin="round";   // форма соединения двух линий
+    //canvas5.lineJoin="bevel";
+    canvas5.lineJoin="miter";
+    canvas5.miterLimit=2;
+    canvas5.bezierCurveTo(x3,y3,x4,y4,x2,y2);
+    //canvas5.moveTo(x2+1,y2+1);
+    canvas5.bezierCurveTo(x3,y3,x4,y4,x1,y1);
 
 
-    
-    //canvas5.rect(130,130,50,50); // перо переносится на начало прямоугольника
-    //canvas5.rect(30,30,50,50); // перо переносится на начало прямоугольника
-    //canvas5.lineTo(40,40);
-    //canvas5.fill();           // соединияе начало и конец и закрашиваем
 
-    canvas5.moveTo(150,100);
-    canvas5.arc(100, 100, 50, 0, Math.PI+3/2*Math.PI, false); // перо необходимо перенести на окружность
 
-    //canvas5.moveTo(250,200);
-    //canvas5.arc(200, 200, 50, 0, 2/3*Math.PI, true); // перо необходимо перенести на окружность
+
+
+
+    canvas5.moveTo(x1,y1);
+    //canvas5.arc(x1,y1,2,0,2*Math.PI);
+
+    canvas5.moveTo(x2,y2);
+    //canvas5.arc(x2,y2,2,0,2*Math.PI);
+
+    canvas5.moveTo(x3,y3);
+    //canvas5.arc(x3,y3,2,0,2*Math.PI);
+
+    canvas5.moveTo(x4,y4);
+    //canvas5.arc(x4,y4,2,0,2*Math.PI);
+
+
 
     canvas5.stroke();
 
