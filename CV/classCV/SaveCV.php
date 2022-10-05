130858ca11a8431a5b1f0cc92e974f384b91f207
dfdxAlex <alexmway@mail.ru>
2022-10-06 00:11:29 +0200

    CV

--- CV/classCV/SaveCV.php
+++ CV/classCV/SaveCV.php
@@ -5,31 +5,44 @@ namespace classCV;
 // класс обслуживает переменную сессии в которой хранится текущий шаг пользователя $_SESSION['level']
 class SaveCV extends \class\nonBD\Button
 {
-    //use class\redaktor\interface\interface;
-
     public function __construct()
     {
+        $this->errorNameCv=false;
         $bd = new \class\redaktor\initBd();
-
-        // проверка присутствия таблиц для модуля работы со статистикой
-        $bd->createTab(
-          'name=cv_json', //имя таблицы для хранения данных CV
-          'poleN=id',     //поле ID
-          'poleT=int',
-          'poleS=0',
-          'poleN=id_user', //поле ID пользователя
-          'poleT=int',
-          'poleS=0',
-          'poleN=json_str',        // поле содержит строку json
-          'poleT=VARCHAR(10000)',
-          'poleS= ',
-        );
-
+        // проверить существует ли таблица для хранения данных, если нет, то создать
+        $this->createTabFoJson($bd);
+        // в переменной хранится имя создаваемой записи
         $this->nameCV='';
         $this->searcValue(); // проверяем была ли еажата кнопка Сохранить, если да, то запомнить данные
+        
+        
+        if ($this->nameCV!='')
+            $this->searchDoubleNameJson($bd);
+        // функция рисует интерфейс для пользователя
+        $this->interfaceForSaveCv();
+    }
 
+    function searchDoubleNameJson($bd)
+    {
+        
+        if ($bd->siearcSlova('cv_json','json_name',$this->nameCV)) {
+            $this->errorNameCv=true;
+        }
+        else {
+             $str=json_encode($_SESSION);
+             $id=$bd->maxIdLubojTablicy("cv_json");
+             $login=$_SESSION['login'];
+             $zapros="INSERT INTO cv_json(id, name_user, json_name, json_str) VALUES ($id,'$login','$this->nameCV','$str')";
+             $bd->zaprosSQL($zapros);
+             }
+
+    }
+
+    function interfaceForSaveCv()
+    {
         // работа с текстами, нуждающимися к переводу
         $nameBlock = new \class\nonBD\Translation('Придумайте название для CV и нажмите кнопку Сохранить');
+        if ($this->errorNameCv) $nameBlock = new \class\nonBD\Translation('Такое имя уже существует, придумайте новое.');
         $nameCv = new \class\nonBD\Translation('Название CV');
         $namePlaceholder = new \class\nonBD\Translation('Название');
         $buttonValue = new \class\nonBD\Translation('Сохранить');
@@ -56,9 +69,26 @@ class SaveCV extends \class\nonBD\Button
     {
         if (!isset($_REQUEST['saveSV'])) return false;
         $this->nameCV = $_REQUEST['nameCV'];
-        //echo $this->nameCV;
     }
 
-
+    function createTabFoJson($bd)
+    {
+        // проверка присутствия таблиц для модуля работы со статистикой
+        $bd->createTab(
+          'name=cv_json', //имя таблицы для хранения данных CV
+          'poleN=id',     //поле ID
+          'poleT=int',
+          'poleS=0',
+          'poleN=name_user', //поле ID пользователя
+          'poleT=VARCHAR(50)',
+          'poleS= ',
+          'poleN=json_name',        // поле содержит имя строки json
+          'poleT=VARCHAR(50)',
+          'poleS= ',
+          'poleN=json_str',        // поле содержит строку json
+          'poleT=VARCHAR(10000)',
+          'poleS= ',
+        );
+    }
 
 }
