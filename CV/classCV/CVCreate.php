298b5da890e99859217e3255c16271ecaf11d494
oleksandr-ishchuk <alexmway@mail.ru>
2024-07-14 18:07:24 +0200

    Loading objects through a loop

--- CV/classCV/CVCreate.php
+++ CV/classCV/CVCreate.php
@@ -3,13 +3,9 @@ namespace classCV;
 
 use \class\nonBD\Translation;
 
-use \classCV\forCvCreate\{
-                          SetPropertySession, UserInfo,
-                          StackTehnologi, CommercialExperience,
-                          Experience, GetPropertyEducation,
-                          CreateLinkForGit, GetPropertySertificates,
-                          GetLanguage, VievCv
-                        };
+use \classCV\forCvCreate\SetPropertySession;
+use \classCV\forCvCreate\VievCv;
+                        
 
 class CVCreate
 {
@@ -30,59 +26,56 @@ class CVCreate
         /**
          * UserInfo($mas); изменяет переданный массив и наполняет его
          * html инфой личных данных. 
-         * Оптимизация кода, замена array_push() 
-         * на array_merge() или array_merge_recursive()
-         * Если всё работает, то при следующей оптимизации удалить
-         * заккоментированный 
          */
-        new UserInfo($mas);
-        // [$addressTeg, $telTeg, $emailTeg, 
-        //  $youtube, $gitHub, $linkdln] = $mas;
-        // array_push($masRez, $addressTeg, $telTeg, $emailTeg, 
-        //            $youtube, $gitHub, $linkdln);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new UserInfo($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
 
-        new StackTehnologi($mas);
-        // [$listSkills,$listSkills_text] = $mas;
-        // array_push($masRez, $listSkills, $listSkills_text);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new StackTehnologi($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
 
-        new CommercialExperience($mas);
-        // [$skills, $skills_text] = $mas;
-        // array_push($masRez, $skills, $skills_text);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new CommercialExperience($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
 
-        new Experience($mas);
-        // [$experience, $experience_text] = $mas;
-        // array_push($masRez, $experience, $experience_text);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new Experience($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
 
-        new GetPropertyEducation($mas);
-        // [$education, $education_text] = $mas;
-        // array_push($masRez, $education, $education_text);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new GetPropertyEducation($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
 
-        new CreateLinkForGit($mas);
-        // [$git, $git_text] = $mas;
-        // array_push($masRez, $git, $git_text);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new CreateLinkForGit($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
 
-        new GetPropertySertificates($mas);
-        // [$certificates, $certificates_text] = $mas;
-        // array_push($masRez, $certificates, $certificates_text);
-        array_push($masRez, ...$mas);
-        unset($mas);
+        // new GetPropertySertificates($mas);
+        // array_push($masRez, ...$mas);
+        // unset($mas);
  
-        new GetLanguage($mas);
-        // [$language, $language_text] = $mas;
-        // array_push($masRez, $language, $language_text);
-        array_push($masRez, ...$mas);
+        // new GetLanguage($mas);
+        // array_push($masRez, ...$mas);
+
+        $classes = [
+            'UserInfo',
+            'StackTehnologi',
+            'CommercialExperience',
+            'Experience',
+            'GetPropertyEducation',
+            'CreateLinkForGit',
+            'GetPropertySertificates',
+            'GetLanguage'
+        ];
+        
+        foreach ($classes as $class) {
+            $mas = [];
+            $class="\\classCV\\forCvCreate\\".$class;
+            new $class($mas);
+            array_push($masRez, ...$mas);
+            unset($mas);
+        }
 
         $servisCreateDFDX = new Translation('CV сгенерировано на моем проекте "cv.php"');
         $masRez[] = $servisCreateDFDX;
